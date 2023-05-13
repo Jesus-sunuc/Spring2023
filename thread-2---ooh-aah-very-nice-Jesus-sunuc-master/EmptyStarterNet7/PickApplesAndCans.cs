8fcec4bd7b135e681e35b5a2758f49b0e9f8adad
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2023-05-13 17:04:00 -0600

    Add files via upload

---
+++ thread-2---ooh-aah-very-nice-Jesus-sunuc-master/EmptyStarterNet7/PickApplesAndCans.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace threads {
+	public class PickApple
+	{
+		// Define a lock object to protect the shared data
+		private object truckLock = new object();
+		// Define the shared data	
+		static int maxAppleInABag = 5;
+		// The number of apples in the truck
+		static int applesInTruck = 0;
+		// The maximum number of apples in the truck
+		static int maxAppleInTheTruck = 50;
+
+		// Define the methods that will be called by the threads
+		public void PickApples(int id)
+		{
+			while (true)
+			{
+				int appleToPick; // Define a variable to save the number of apples to pick
+				lock (truckLock) // Lock the truck
+				{
+					if (applesInTruck == maxAppleInTheTruck)	// Check if the truck is full
+					{
+						break; 		// If the truck is full, break out of the loop
+					}
+
+					appleToPick = Math.Min(maxAppleInABag, maxAppleInTheTruck - applesInTruck); // Calculate the number of apples to pick
+					applesInTruck += appleToPick; // Add the number of apples to pick to the truck
+					Console.WriteLine($"Picker {id} has picked {appleToPick} apples. The truck has: {applesInTruck}"); // Print the number of apples picked and the number of apples in the truck
+				}
+			}
+		}
+		public void CanApples(int id) // Define the method that will be called by the canner threads
+		{
+			while (true)
+			{
+				int applesToCan; // Define a variable to save the number of apples to can
+				lock (truckLock) // Lock the truck
+				{
+					if (applesInTruck == 0)	// Check if the truck is empty
+					{
+						break;	// If the truck is empty, break out of the loop
+					}
+
+					applesToCan = Math.Min(maxAppleInABag, applesInTruck); // Calculate the number of apples to can
+					applesInTruck -= applesToCan; // Subtract the number of apples to can from the truck
+				}
+					Console.WriteLine($"Canner {id} has canned {applesToCan} apples. Apples in the truck: {applesInTruck}"); // Print the number of apples canned and the number of apples in the truck
+			}
+		}
+	}
+}
---
+++ thread-2---ooh-aah-very-nice-Jesus-sunuc-master/EmptyStarterNet7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace threads {
+    class Program
+    {
+        static int pickers = 3; // Define the number of picker threads
+        static int canners = 5; // Define the number of canner threads
+
+        public static void Main()
+        {
+            PickApple pa = new PickApple();
+            // Create the picker threads
+            Thread[] pickerThreads1 = new Thread[pickers];
+            for (int i = 0; i < pickers; i++)
+            {
+                pickerThreads1[i] = new Thread(() => pa.PickApples(i));
+                pickerThreads1[i].Start();
+            }
+
+            // Create the canner threads
+            Thread[] cannerThreads = new Thread[canners];
+            for (int i = 0; i < canners; i++)
+            {
+                cannerThreads[i] = new Thread(() => pa.CanApples(i));
+                cannerThreads[i].Start();
+            }
+
+            // Wait for all the threads to finish
+            foreach (Thread t in pickerThreads1)
+            {
+                t.Join();
+            }
+      
+            foreach (Thread t in cannerThreads)
+            {
+                t.Join();
+            }
+        }
+    }
+}
